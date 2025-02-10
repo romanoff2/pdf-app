7f62a721435bca6f9006094a3889d81fc123d20a
romanoff2 <91600790+romanoff2@users.noreply.github.com>
2025-02-10 16:45:29 +0100

    Added bill parser

---
+++ api/bill.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+const getRawBody = require('raw-body');
+const FormData = require('form-data');
+
+module.exports.config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-File-Name, Authorization');
+
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
+
+  try {
+    const buffer = await getRawBody(req);
+    const fileName = req.headers['x-file-name'] || 'bill.pdf';
+    const contentType = req.headers['content-type'];
+    const authHeader = req.headers['authorization'];
+
+    const form = new FormData();
+    form.append('file', buffer, {
+      filename: fileName,
+      contentType: contentType,
+    });
+
+    const headers = {
+      ...form.getHeaders(),
+      ...(authHeader ? { 'Authorization': authHeader } : {})
+    };
+
+    const apiUrl = 'https://europe-west8-scriba-1.cloudfunctions.net/bill';
+    const response = await axios.post(apiUrl, form, { headers });
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Full error:', error);
+    res.status(500).json({
+      error: error.message,
+      details: error.response ? error.response.data : null,
+      requestInfo: {
+        fileName: req.headers['x-file-name'],
+        contentType: req.headers['content-type']
+      }
+    });
+  }
+};
